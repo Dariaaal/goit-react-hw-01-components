6dfc6e661719dfc43e35882de8119eae1824a8ed
Dariaaal <1406dariaal@gmail.com>
2023-03-06 14:25:00 +0200

    style

--- src/components/transactions/TransactionHistory.jsx
+++ src/components/transactions/TransactionHistory.jsx
@@ -3,15 +3,15 @@ import css from './TransactionHistory.module.css';
 
 function Transactions({items}){
     return <table>
-    <thead>
+    <thead className={css.thead}>
       <tr>
-        <th>Type</th>
-        <th>Amount</th>
-        <th>Currency</th>
+        <th className={css.label}>Type</th>
+        <th className={css.label}>Amount</th>
+        <th className={css.label}>Currency</th>
       </tr>
     </thead>
   
-    <tbody>
+    <tbody className={css.tbody}>
         {items.map(item=>(
           <TransactionItem
           key={item.id}
